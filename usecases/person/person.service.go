bb3d1e23ac8e0514307c2685ec42e7425db3c054
willjrcom <williamjunior67@gmail.com>
2023-07-29 10:08:19 -0300

    feat: :art: person service added

--- usecases/person/person.service.go
+++ usecases/person/person.service.go
@@ -1,19 +1,36 @@
 package personUsecases
 
 import (
-	"fmt"
-	addressEntity "projetoGo/entity/address"
 	personEntity "projetoGo/entity/person"
+	repositoryInterface "projetoGo/entity/repository"
 )
 
-func RegisterPerson() {
-	addressBuilder := addressEntity.AddressBuilder{}
-	address := addressBuilder.WithStreet("Rua Piedade").WithNumber(226).WithCity("Sorocaba").WithState("SP").WithCountry("BR").Build()
-	person, err := personEntity.NewPersonBuilder("William", "436.377.998-55").WithAddress(address).Build()
+type Service struct {
+	Repository repositoryInterface.Repository[personEntity.Person]
+}
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(person)
+func (s *Service) FindAll() ([]*personEntity.Person, error) {
+	return s.Repository.FindAll()
+}
+
+func (s *Service) FindBy() ([]*personEntity.Person, error) {
+	people, err := s.Repository.FindAll()
+	return people, err
+}
+
+func (s *Service) RegisterPerson(newPerson *personEntity.Person) {
+	s.Repository.Save(newPerson)
+	// addressBuilder := addressEntity.AddressBuilder{}
+	// address := addressBuilder.WithStreet("Rua Piedade").WithNumber(226).WithCity("Sorocaba").WithState("SP").WithCountry("BR").Build()
+	// person, err := personEntity.NewPersonBuilder("William", "436.377.998-55").WithAddress(address).Build()
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+}
+
+func (s *Service) DeletePerson(params string) {
+	// !!! Criar regra do where
+	s.Repository.DeleteBy(params, []string{"will"})
 }
