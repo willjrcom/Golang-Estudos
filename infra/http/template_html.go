1306ca9704bcf87dfd36fd33c6e8242e94853a01
willjrcom <williamjunior67@gmail.com>
2023-08-01 15:32:15 -0300

    refactor:

--- infra/http/template_html.go
+++ infra/http/template_html.go
@@ -8,8 +8,8 @@ import (
 var modelos = template.Must(template.ParseFiles("infra/html/index.html"))
 
 func templateHtml(w http.ResponseWriter, r *http.Request) {
-	people, _ := Service.FindAll()
-	err := modelos.ExecuteTemplate(w, "index.html", people)
+	people, err := Service.FindAll()
+	err = modelos.ExecuteTemplate(w, "index.html", people)
 
 	if err != nil {
 		http.Error(w, "Erro ao carregar template", http.StatusInternalServerError)
